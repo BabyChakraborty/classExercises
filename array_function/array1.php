1bdc6d18e07c6f50782c73846f1dcbece3304bd6
Baby Chakraborty <babycu07@gmail.com>
2014-12-11 17:04:15 +0600

    array1.php

--- array_function/array1.php
+++ array_function/array1.php
@@ -1,8 +1,4 @@
-
 <?php
-$fruits = array (
-    "fruits"  => array("a" => "orange", "b" => "banana", "c" => "apple"),
-    "numbers" => array(1, 2, 3, 4, 5, 6),
-    "holes"   => array("first", 5 => "second", "third")
-);
+$input_array = array("FirSt" => 1, "SecOnd" => 4);
+print_r(array_change_key_case($input_array, CASE_UPPER));
 ?>
